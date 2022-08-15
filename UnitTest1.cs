9429c497fd97ccbd13cf5f82dae100a4b49714a8
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2022-08-15 17:56:03 +0100

    Unit Test using NUnit.Framework

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace NUnitTest;
+public class Tests
+{
+    
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(new string[] { "ab", "c_" }, TestLibrary.Class1.Solution2("abc"));
+        Assert.AreEqual(new string[] { "ab", "cd", "ef" }, TestLibrary.Class1.Solution2("abcdef"));
+    }
+    
+
+
+    /*
+    [Test]
+    public void Test2()
+    {
+        Assert.AreEqual(1, TestLibrary.Class1.test());
+    }
+    
+*/
+
+}
