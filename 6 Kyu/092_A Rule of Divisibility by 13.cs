fb142ce644d3d55d7fd3bc4d1be15b97c1dac3e5
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2023-09-01 05:58:48 +0100

    Create 092_A Rule of Divisibility by 13.cs

---
+++ 6 Kyu/092_A Rule of Divisibility by 13.cs
@@ -0,0 +1,123 @@
+/*Challenge link:https://www.codewars.com/kata/564057bc348c7200bd0000ff/train/csharp
+Question:
+From Wikipedia:
+
+"A divisibility rule is a shorthand way of determining whether a given integer is divisible by a fixed divisor without performing the division, usually by examining its digits."
+
+When you divide the successive powers of 10 by 13 you get the following remainders of the integer divisions:
+
+1, 10, 9, 12, 3, 4 because:
+
+10 ^ 0 ->  1 (mod 13)
+10 ^ 1 -> 10 (mod 13)
+10 ^ 2 ->  9 (mod 13)
+10 ^ 3 -> 12 (mod 13)
+10 ^ 4 ->  3 (mod 13)
+10 ^ 5 ->  4 (mod 13)
+(For "mod" you can see: https://en.wikipedia.org/wiki/Modulo_operation)
+
+Then the whole pattern repeats. Hence the following method:
+
+Multiply
+
+the right most digit of the number with the left most number in the sequence shown above,
+the second right most digit with the second left most digit of the number in the sequence.
+The cycle goes on and you sum all these products. Repeat this process until the sequence of sums is stationary.
+
+Example:
+What is the remainder when 1234567 is divided by 13?
+
+7      6     5      4     3     2     1  (digits of 1234567 from the right)
+×      ×     ×      ×     ×     ×     ×  (multiplication)
+1     10     9     12     3     4     1  (the repeating sequence)
+Therefore following the method we get:
+
+ 7×1 + 6×10 + 5×9 + 4×12 + 3×3 + 2×4 + 1×1 = 178
+
+We repeat the process with the number 178:
+
+8x1 + 7x10 + 1x9 = 87
+
+and again with 87:
+
+7x1 + 8x10 = 87
+
+From now on the sequence is stationary (we always get 87) and the remainder of 1234567 by 13 is the same as the remainder of 87 by 13 ( i.e 9).
+
+Task:
+Call thirt the function which processes this sequence of operations on an integer n (>=0). thirt will return the stationary number.
+
+thirt(1234567) calculates 178, then 87, then 87 and returns 87.
+
+thirt(321) calculates 48, 48 and returns 48
+*/
+
+//***************Solution********************
+
+public class Thirteen{
+  public static long Thirt(long n){
+    //repeat pattern
+      int[] seq = { 1, 10, 9, 12, 3, 4 };
+      long sum = 0;
+    
+      for (var i = 0; n > 0; i++){
+        //% 6 because seq legnth is 6, n % 10 to get last digit
+          sum += n % 10 * seq[i % 6];
+        //n / 10 to get next digit, 1234567 / 10 becomes 123456
+          n /= 10;
+      }
+    
+    //if sum is greater than 99, pass sum into recurisive method, else return sum
+      return sum > 99 ? Thirt(sum) : sum;
+  }
+}
+
+//****************Sample Test*****************
+using System;
+using NUnit.Framework;
+
+[TestFixture]
+public static class ThirteenTests {
+
+    private static Random rnd = new Random();
+
+    private static void testing(long actual, long expected) {
+        Assert.AreEqual(expected, actual);
+    }
+
+[Test]
+    public static void test1() {
+        Console.WriteLine("Testing Thirt");
+        testing(Thirteen.Thirt(8529), 79);
+        testing(Thirteen.Thirt(85299258), 31);
+        testing(Thirteen.Thirt(5634), 57);
+        testing(Thirteen.Thirt(1111111111), 71);
+        testing(Thirteen.Thirt(987654321), 30);
+    }
+    
+    //-----------------------
+    public static long ThirtSol(long n) {
+        int[] w = new int[] {1, 10, 9, 12, 3, 4};
+        while (true) {
+            long r = n; long q = -1; long c = 0; int j = 0;
+            while (q != 0) {
+                q = (long)(r / 10);
+                c += (r % 10) * w[j % 6];
+                r = q;
+                j++;
+            }
+            if (c == n) 
+                return c;
+            n = c;
+        }
+    }
+    //-----------------------
+[Test]    
+    public static void RandomTest() {
+        Console.WriteLine("Random Tests");
+        for (int i = 0; i < 200; i++) { 
+            int n = rnd.Next(150, 1500000000);
+            testing(Thirteen.Thirt(n), ThirtSol(n)); 
+        }
+    }
+}  
