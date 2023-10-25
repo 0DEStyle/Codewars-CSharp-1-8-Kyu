ed1fd3840831f2ff779f4e18b4e3b6fa4a8c4608
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2023-10-25 07:55:29 +0100

    Create Ookkk, Ok, O? Ook, Ok, Ooo!.cs

---
+++ Funny Code/Ookkk, Ok, O? Ook, Ok, Ooo!.cs
@@ -0,0 +1,89 @@
+//Link: https://www.codewars.com/kata/55035eb47451fb61c0000288/solutions/csharp
+
+
+using OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOk = System;
+using OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookOokkkkOokOkkkOkOoOkkOokOk = System.Byte;
+using OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk = System.String;
+using OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOkOoOkkOokOkOkkkkOooOkkkOkOoOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOkOoookOokkkOkOkOkkOkOokOkkOkkOoOkkOokOoOkkOokOkOkkkOokO = System.Text.StringBuilder;
+using OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOokOokOkkOkkkOokkkOkOoOokkOokkOokkOokO = System.Int32;
+using OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookkOkkOkOooOkkOoookOkkkOokO = System.Char;
+using OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOkOoOkkOokOkOkkkkOooOkkkOkOoOokOkkkOokOookOkOkkOkkkOokkOookkOkkOkkkkOkkOokOoOkkOkOokOkkOkkkOokkOokkk = System.Text.Encoding;
+using OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookkOkkOkkkkOkkOkkOoOkkOkkOoOkkOokOkOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkkOokkOokOkkkOokOookkkOkkOokOkOkkOkkkOokkOokOkOkkkOokOokkOkOokOkkOookkOokOkkkOokOookOoOkkOkOokOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkOoookOkkkOokOokkkkOokOokkkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokOkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokkkkkO = System.Collections.Generic.Dictionary<string,string>;
+using OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookOokkOkkkkOkkOkkkkOkkOkkOoOkkOokOkOkkOoookOkkOkkkO = System.Boolean;
+
+public static class Kata
+{
+    public static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkOokOo(OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk okkOookk)
+    {
+        OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOkOoOkkOokOkOkkkkOooOkkkOkOoOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOkOoookOokkkOkOkOkkOkOokOkkOkkOoOkkOokOoOkkOokOkOkkkOokO OkkkOokkOkkOookO = new OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOkOoOkkOokOkOkkkkOooOkkkOkOoOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOkOoookOokkkOkOkOkkOkOokOkkOkkOoOkkOokOoOkkOokOkOkkkOokO();
+        OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookkOkkOkkkkOkkOkkOoOkkOkkOoOkkOokOkOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkkOokkOokOkkkOokOookkkOkkOokOkOkkOkkkOokkOokOkOkkkOokOokkOkOokOkkOookkOokOkkkOokOookOoOkkOkOokOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkOoookOkkkOokOokkkkOokOokkkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokOkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokkkkkO();
+        okkOookk = okkOookk.OkOkOkOoOkkOkkkkOkOokkOoOkkOkkkkOkkkOkkkOkkOokOkOkkkOokO();
+        okkOookk = okkOookk.OkOkOokOokkOokOkOkkkOoooOkkOkkOoOkkOoookOkkOookkOkkOokOk("Okk, Ok, Okk!", "Ookk, Oook!");
+        okkOookk = okkOookk.OkOkOokOokkOokOkOkkkOoooOkkOkkOoOkkOoookOkkOookkOkkOokOk("Okk, Okkkk!", "Ookk, Oooo!");
+        okkOookk = okkOookk.OkOkOokOokkOokOkOkkkOoooOkkOkkOoOkkOoookOkkOookkOkkOokOk("Ook, Okk, Oo!", "");
+        okkOookk = okkOookk.OkOkOokOokkOokOkOkkkOoooOkkOkkOoOkkOoookOkkOookkOkkOokOk("Ook, Ooooo!", "");
+        okkOookk = okkOookk.OkOkOokOokkOokOkOkkkOoooOkkOkkOoOkkOoookOkkOookkOkkOokOk("Ook, Ooook!", "");
+
+        OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk[] OkkOookOokOooook
+          = okkOookk.OkOkOokkOkkkOoooOkkOkkOoOkkOkOokOkkkOkOo(OkkOokOoOkkOkOokOkkOookkOkkkOkOo["Ookkkkkk!"][0]);
+          
+        foreach (OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkOookO
+            in OkkOookOokOooook)
+        {
+            OkkkOokkOkkOookO.OkOkOokkOkkkOoooOkkOkkOoOkkOkOokOkkkOkOo(
+                OkOookOkOkkOkkkOokkOookkOkkOkkkkOkkOokOoOkkOkOokOkkOkkkOokkOokkkOokOkkkOokOooookOkOkOokkOkOoookkOkOokOokOkOokOokOokOkkkOokOookkkOkkOokOkOkkkOkOoOkOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk(
+                    new OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookOokkkkOokOkkkOkOoOkkOokOk[] {
+                            OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookkOkkOkkkkOkkOkkkOokkkOkkOokkOokOkOkkkOokOokkkOkOoOokOkkkOokOkOkOoOkkOkkkkOkOoookOokkkkOokOkkkOkOoOkkOokOk(OkkOookO, 2)
+                    }
+                ));
+        }
+
+        return OkkkOokkOkkOookO.OkOkOkOoOkkOkkkkOkOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk();
+    }
+
+    static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookkOkkOkkkkOkkOkkOoOkkOkkOoOkkOokOkOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkkOokkOokOkkkOokOookkkOkkOokOkOkkOkkkOokkOokOkOkkkOokOokkOkOokOkkOookkOokOkkkOokOookOoOkkOkOokOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkOoookOkkkOokOokkkkOokOokkkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokOkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokkkkkO
+  OkkOokOoOkkOkOokOkkOookkOkkkOkOo = new OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookkOkkOkkkkOkkOkkOoOkkOkkOoOkkOokOkOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkkOokkOokOkkkOokOookkkOkkOokOkOkkOkkkOokkOokOkOkkkOokOokkOkOokOkkOookkOokOkkkOokOookOoOkkOkOokOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkOoookOkkkOokOokkkkOokOokkkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokOkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokkkkkO();
+
+    static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkOkOkOoOkkOkkkkOkOokkOoOkkOkkkkOkkkOkkkOkkOokOkOkkkOokO(this OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkkOokk) { return OkkkOokk.ToLower(); }
+    static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkOkOokOokkOokOkOkkkOoooOkkOkkOoOkkOoookOkkOookkOkkOokOk(this OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkkOokk, OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkOoook, OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkOookO) { return OkkkOokk.Replace(OkkOokOoOkkOkOokOkkOookkOkkkOkOo[OkkOoook], OkkOokOoOkkOkOokOkkOookkOkkkOkOo[OkkOookO]); }
+    static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk[] OkOkOokkOkkkOoooOkkOkkOoOkkOkOokOkkkOkOo(this OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkkOokk, OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookkOkkOkOooOkkOoookOkkkOokO OkkOookk) { return OkkkOokk.Split(OkkOookk); }
+    static void OkOkOokkOkkkOoooOkkOkkOoOkkOkOokOkkkOkOo(this OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOkOoOkkOokOkOkkkkOooOkkkOkOoOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOkOoookOokkkOkOkOkkOkOokOkkOkkOoOkkOokOoOkkOokOkOkkkOokO OkkkOokkOkkOookO, OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkkOokk) { OkkkOokkOkkOookO.Append(OkkkOokk); }
+    static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkOkOkOoOkkOkkkkOkOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk(this OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOkOoOkkOokOkOkkkkOooOkkkOkOoOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOkOoookOokkkOkOkOkkOkOokOkkOkkOoOkkOokOoOkkOokOkOkkkOokO OkkkOokkOkkOookO) { return OkkkOokkOkkOookO.ToString(); }
+    static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkOookOkOkkOkkkOokkOookkOkkOkkkkOkkOokOoOkkOkOokOkkOkkkOokkOokkkOokOkkkOokOooookOkOkOokkOkOoookkOkOokOokOkOokOokOokOkkkOokOookkkOkkOokOkOkkkOkOoOkOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk(OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookOokkkkOokOkkkOkOoOkkOokOk[] OkkOookO) { return OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOkOoOkkOokOkOkkkkOooOkkkOkOoOokOkkkOokOookOkOkkOkkkOokkOookkOkkOkkkkOkkOokOoOkkOkOokOkkOkkkOokkOokkk.ASCII.OkOookkkOkkOokOkOkkkOkOoOkOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk(OkkOookO); }
+    static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkOookkkOkkOokOkOkkkOkOoOkOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk(this OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOkOoOkkOokOkOkkkkOooOkkkOkOoOokOkkkOokOookOkOkkOkkkOokkOookkOkkOkkkkOkkOokOoOkkOkOokOkkOkkkOokkOokkk OkkOokOkOkkOkkkOokkOookkOkkOkkkkOkkOokOoOkkOkOokOkkOkkkOokkOokkk, OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookOokkkkOokOkkkOkOoOkkOokOk[] OkkOookO) { return OkkOokOkOkkOkkkOokkOookkOkkOkkkkOkkOokOoOkkOkOokOkkOkkkOokkOokkk.GetString(OkkOookO); }
+    static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookOokkkkOokOkkkOkOoOkkOokOk OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookkOkkOkkkkOkkOkkkOokkkOkkOokkOokOkOkkkOokOokkkOkOoOokOkkkOokOkOkOoOkkOkkkkOkOoookOokkkkOokOkkkOkOoOkkOokOk(OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkkOokk, OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOokOokOkkOkkkOokkkOkOoOokkOokkOokkOokO OkkOkOok) { return OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOk.Convert.ToByte(OkkkOokk, OkkOkOok); }
+    static OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookOokkOkkkkOkkOkkkkOkkOkkOoOkkOokOkOkkOoookOkkOkkkO OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOookOkOkkOkkOkOkkkOoooOkkkOkOoOkkkkOok() { return OkkOokOoOkkOkOokOkkOookkOkkkOkOo.Count == 0; }
+    static void OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo(OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkOkOkk, OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkk OkkkOkkO) { OkkOokOoOkkOkOokOkkOookkOkkkOkOo.Add(OkkOkOkk, OkkkOkkO); }
+
+    static void OkOkOokkOkkkkOokOkkkOokkOkkkOkOoOkkOokOkOkkOkkOkOokOkkkOokOoookkOkkOkkkkOkkOkkOoOkkOkkOoOkkOokOkOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkkOokkOokOkkkOokOookkkOkkOokOkOkkOkkkOokkOokOkOkkkOokOokkOkOokOkkOookkOokOkkkOokOookOoOkkOkOokOkkOookkOkkkOkOoOkkOkOokOkkOkkkkOkkOkkkOokkOoookOkkkOokOokkkkOokOokkkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokOkkOoOkkkOokkOkkkOkOoOkkkOokOokkOkOokOkkOkkkOokkOokkkOokkkkkO()
+    {
+        if (OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOookOkOkkOkkOkOkkkOoooOkkkOkOoOkkkkOok())
+        {
+            OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo("Okk, Ok, Okk!", "k");
+            OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo("Okk, Okkkk!", "o");
+            OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo("Ook, Okk, Oo!", ",");
+            OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo("Ook, Ooooo!", " ");
+            OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo("Ook, Ooook!", "!");
+            OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo("Ookk, Oook!", "1");
+            OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo("Ookk, Oooo!", "0");
+            OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo("Ookkkkkk!", "?");
+            OkkOokOoOkkOkOokOkkOookkOkkkOkOoOkOooookOkkOokOoOkkOokOo("", "");
+        }
+    }
+}
+
+    /*
+        Okk, Ok, Okk?  Okk, Ok, Ook?  Okk, Okk, Oo?  Okk, Okk, Oo?  Ook, Ooooo?  Okk, Okk, Ok?  Okk, Ook, Ok!
+
+        Ok, Ook, Ook?  Ook, Ooooo?  Okkk, Okkk?  Okk, Ok, Ook?  Okkk, Ookk?  Okk, Ok, Ooo?  Ook, Ooooo?  Ok, Ooookk?  Ook, Oookk?  Ook, Ooooo?  Okkk, Okkk?  Okk, Okkkk?
+        Okkk, Ok, Ok?  Okk, Okk, Oo?  Okk, Ook, Oo?  Ook, Ooooo?  Okk, Okk, Oo?  Okk, Ook, Ok?  Okkk, Ok, Oo?  Ook, Ooooo?  Okk, Okk, Ok?  Okk, Ook, Ok?  Ook, Ooooo?
+        Okkk, Oooo?  Okk, Okk, Oo?  Okk, Ooook?  Okk, Oookk?  Okk, Ook, Ok?  Ook, Ooooo?  Okkk, Ok, Ok?  Okkk, Ookk?  Okk, Ok, Ook?  Okk, Okkk, O?  Okk, Ookkk?  Okkk,
+        Ookk?  Ook, Ooooo?  Okk, Ooook?  Okkk, Ok, Oo?  Ook, Ooooo?  Okkk, Ok, Oo?  Okk, Ok, Ooo?  Okk, Ook, Ok?  Ook, Ooooo?  Okk, Oook, O?  Okk, Okkkk?  Okkk, Ok, Oo?
+        Okkk, Ok, Oo?  Okk, Okkkk?  Okk, Okk, Ok?  Ook, Ooooo?  Okk, Ok, Ook?  Okk, Okkk, O?  Okkk, Ookk?  Okkk, Ok, Oo?  Okk, Ook, Ok?  Okk, Ooook?  Okk, Ook, Oo?  Ook, 
+        Ooooo?  Okk, Okkkk?  Okk, Ookk, O?  Ook, Ooooo?  Okkk, Ok, Oo?  Okk, Ok, Ooo?  Okk, Ook, Ok?  Ook, Ooooo?  Okkk, Ok, Oo?  Okk, Okkkk?  Okkk, Oooo!
+
+        Ok, Ook, Ook?  Okkk, Ok, Oo?  Ook, Ooooo?  Okk, Ok, Okk?  Okk, Ok, Ook?  Okk, Okk, Oo?  Okk, Okk, Oo?  Okkk, Ookk?  Ook, Ooooo?  Okk, Ok, Ooo?  Okk, Ooook?  Okk,
+        Okk, Oo?  Okk, Ookk, O?  Ook, Ooooo?  Okkk, Ok, Oo?  Okk, Ok, Ooo?  Okk, Ook, Ok?  Ook, Ooooo?  Okk, Okk, Ok?  Okkkk, Ook?  Okkk, Ookk?  Okkk, Ok, Oo?  Okk, Ook, 
+        Ok?  Okkk, Ook, O?  Okkkk, Ook?  Ook, Ooooo?  Okk, Okkkk?  Okkk, Ok, Oo?  Okk, Ok, Ooo?  Okk, Ook, Ok?  Okkk, Ook, O?  Okkk, Okkk?  Okk, Ok, Ook?  Okkk, Ookk?  
+        Okk, Ook, Ok?  Ook, Ooooo?  Ookkk, Ok, O?  Ook, Okkkk!
+    */
