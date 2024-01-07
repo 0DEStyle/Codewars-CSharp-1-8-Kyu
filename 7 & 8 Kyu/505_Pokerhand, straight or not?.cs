0894104e1836d78a836134adf41ad139eaf96b7a
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-01-07 11:18:56 +0000

    Create 505_Pokerhand, straight or not?.cs

---
+++ 7 & 8 Kyu/505_Pokerhand, straight or not?.cs
@@ -0,0 +1,133 @@
+/*Challenge link:https://www.codewars.com/kata/582afcadac2d9baa0900054c/train/csharp
+Question:
+Texas Hold-em!
+
+Your task is to determine if the cards in the list makes up a straight (five cards of sequential rank) or not. The cards will always have values ranging from 2-14, where 14 is the ace.
+
+Be aware that the ace (14) also should count as value 1!
+
+The number of cards will vary, but will never be more than 7 (the board (5) + player hand (2))
+
+Examples:
+
+straight: 9-10-11-12-13
+
+straight: 14-2-3-4-5
+
+straight: 2-7-8-5-10-9-11
+
+not straight: 7-8-12-13-14
+
+
+*/
+
+//***************Solution********************
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⢀⣴⠟⠉⠀⠀⠀⠈⠻⣦⡀⠀⠀⠀⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣀⢀⣾⠿⠻⢶⣄⠀⠀⣠⣶⡿⠶⣄⣠⣾⣿⠗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⢻⣿⣿⡿⣿⠿⣿⡿⢼⣿⣿⡿⣿⣎⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⠉⠛⢛⣛⡉⠀⠀⠙⠛⠻⠛⠑⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣧⣤⣴⠿⠿⣷⣤⡤⠴⠖⠳⣄⣀⣹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣀⣟⠻⢦⣀⡀⠀⠀⠀⠀⣀⡈⠻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡿⠉⡇⠀⠀⠛⠛⠛⠋⠉⠉⠀⠀⠀⠹⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡟⠀⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠈⠑⠪⠷⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣦⣼⠛⢦⣤⣄⡀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠢⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠲⠖⠛⠻⣿⡿⠛⠉⠉⠻⠷⣦⣽⠿⠿⠒⠚⠋⠉⠁⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣾⠛⠁⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⠀⠀⠀
+⠀⠀⠀⠀⣰⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣑⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⠀⠀
+⠀⠀⠀⣰⣿⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣧⣄⠀⠀⠀⠀⠀⠀⢳⡀⠀
+⠀⠀⠀⣿⡾⢿⣀⢀⣀⣦⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⣫⣿⡿⠟⠻⠶⠀⠀⠀⠀⠀⢳⠀
+⠀⠀⢀⣿⣧⡾⣿⣿⣿⣿⣿⡷⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⢀⡴⢿⣿⣧⠀⡀⠀⢀⣀⣀⢒⣤⣶⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⠀⡾⠁⠙⣿⡈⠉⠙⣿⣿⣷⣬⡛⢿⣶⣶⣴⣶⣶⣶⣤⣤⠤⠾⣿⣿⣿⡿⠿⣿⠿⢿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⣸⠃⠀⠀⢸⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⠟⡉⠀⠀⠀⠈⠙⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⣿⠀⠀⢀⡏⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀⠀⠉⠠⠿⠟⠻⠟⠋⠉⢿⣿⣦⡀⢰⡀⠀⠀⠀⠀⠀⠀⠁
+⢀⣿⡆⢀⡾⠀⠀⠀⠀⣾⠏⢿⣿⣿⣿⣯⣙⢷⡄⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣻⢿⣷⣀⣷⣄⠀⠀⠀⠀⢸⠀
+⢸⠃⠠⣼⠃⠀⠀⣠⣾⡟⠀⠈⢿⣿⡿⠿⣿⣿⡿⠿⠿⠿⠷⣄⠈⠿⠛⠻⠶⢶⣄⣀⣀⡠⠈⢛⡿⠃⠈⢿⣿⣿⡿⠀⠀⠀⠀⠀⡀
+⠟⠀⠀⢻⣶⣶⣾⣿⡟⠁⠀⠀⢸⣿⢅⠀⠈⣿⡇⠀⠀⠀⠀⠀⣷⠂⠀⠀⠀⠀⠐⠋⠉⠉⠀⢸⠁⠀⠀⠀⢻⣿⠛⠀⠀⠀⠀⢀⠇
+⠀⠀⠀⠀⠹⣿⣿⠋⠀⠀⠀⠀⢸⣧⠀⠰⡀⢸⣷⣤⣤⡄⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡆⠀⠀⠀⠀⡾⠀⠀⠀⠀⠀⠀⢼⡇
+⠀⠀⠀⠀⠀⠙⢻⠄⠀⠀⠀⠀⣿⠉⠀⠀⠈⠓⢯⡉⠉⠉⢱⣶⠏⠙⠛⠚⠁⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⠀⠀⠀⠀⠀⠻⠄⠀⠀⠀⢀⣿⠀⢠⡄⠀⠀⠀⣁⠁⡀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⢀⣐⡟⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⠀⠀⠀
+*/
+using System.Collections.Generic;
+using System.Linq;
+  
+namespace Poker {
+    public static class PokerHand{
+        public static bool IsStraight(List<Card> cards){
+          //if 14 exist, add 1 to the cards List.
+          if (cards.Any(x => x.CardValue == 14))
+            cards.Add(new Card{CardValue = 1});
+          //generate sequence, start from 1, count up to 14
+          //x and y are current elements
+          //if the current x(index) matches any card value y, return T(true), else F(False), convert the result to array
+          //check if all 5 hands are true and return a bool value.
+          return new string(Enumerable.Range(1, 14).Select(x => cards.Any(y => y.CardValue == x) ? 'T' : 'F').ToArray())
+                                              .Contains("TTTTT");
+          }
+    }
+    public class Card {
+        public int CardValue { get; set; }
+    }
+}
+
+//****************Sample Test*****************
+namespace Poker {
+  using NUnit.Framework;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  
+  [TestFixture]
+  public class PokerHandTest
+  {
+    private static Random rng = new Random(DateTime.Now.Ticks.GetHashCode());
+    
+    [TestCase(new[] { 2, 3, 4 ,5, 6}, true, "2, 3, 4 ,5, 6")]
+    [TestCase(new[] { 14, 5, 4 ,2, 3}, true, "14, 5, 4 ,2, 3")]
+    [TestCase(new[] { 2, 3}, false, "2, 3")]
+    [TestCase(new[] { 7, 7, 12 ,11, 3, 4, 14}, false, "7, 7, 12 ,11, 3, 4, 14")]
+    [TestCase(new[] { 2, 7, 12 ,11, 3, 4, 14}, false, "2, 7, 12 ,11, 3, 4, 14")]
+    [TestCase(new[] { 10, 7, 12 ,11, 13, 4, 14}, true, "10, 7, 12 ,11, 13, 4, 14")]
+    [TestCase(new[] { 10, 14, 14 ,13, 12, 10, 11}, true, "10, 14, 14 ,13, 12, 10, 11")]
+    [TestCase(new[] { 2, 7, 12 ,11, 10, 13, 14}, true, "2, 7, 12 ,11, 10, 13, 14")]
+    [TestCase(new[] { 3, 4, 5 ,6, 8, 9, 10}, false, "")]
+    public void IsStraight(IEnumerable<int> cardValues, bool isStraight, string errorMessage)
+    {
+      var cards = new List<Card>();
+      foreach(var cardValue in cardValues) {
+        cards.Add(new Card { CardValue = cardValue });
+      }
+      
+      Assert.AreEqual(isStraight, PokerHand.IsStraight(cards), errorMessage); 
+    }
+    
+    [Test]
+    public void IsStraight_AllVariations() {
+      var cardValues = new[] { 14, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+      foreach(var value in cardValues) {
+        var range = Enumerable.Range(value == 14 ? 2 : value +1, 4).ToList();
+        range.Add(value);
+        range.Add(rng.Next(2, 14));
+        range.Add(rng.Next(2, 14));
+        range = Shuffle(range);
+        var cards = range.Select(r => new Card { CardValue = r }).ToList();
+        
+        Assert.IsTrue(PokerHand.IsStraight(cards), string.Join(", ", range.Select(r => r)));
+      }
+    }
+      
+      private List<int> Shuffle(List<int> list)  
+      {  
+        var n = list.Count;  
+        while (n > 1) {  
+          n--;  
+          var k = rng.Next(n + 1);  
+          var value = list[k];  
+          list[k] = list[n];  
+          list[n] = value;  
+        }  
+        return list;
+      }
+    }
+  }
