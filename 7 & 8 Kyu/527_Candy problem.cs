82a8f7b2dfd76ad2066bce683e5a0422b5e95668
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-01-11 08:48:49 +0000

    Create 527_Candy problem.cs

---
+++ 7 & 8 Kyu/527_Candy problem.cs
@@ -0,0 +1,184 @@
+/*Challenge link:https://www.codewars.com/kata/55466644b5d240d1d70000ba/train/csharp
+Question:
+Description
+"It's the end of trick-or-treating and we have a list/array representing how much candy each child in our group has made out with. We don't want the kids to start arguing, and using our parental intuition we know trouble is brewing as many of the children in the group have received different amounts of candy from each home.
+
+So we want each child to have the same amount of candies, only we can't exactly take any candy away from the kids, that would be even worse. Instead we decide to give each child extra candy until they all have the same amount.
+
+Task
+Your job is to find out how much candy each child has, and give them each additional candy until they too have as much as the child(ren) with the most candy. You also want to keep a total of how much candy you've handed out because reasons."
+
+Your job is to give all the kids the same amount of candies as the kid with the most candies and then return the total number candies that have been given out. If there are no kids, or only one, return -1.
+
+In the first case (look below) the most candies are given to second kid (i.e second place in list/array), 8. Because of that we will give the first kid 3 so he can have 8 and the third kid 2 and the fourth kid 4, so all kids will have 8 candies.So we end up handing out 3 + 2 + 4 = 9.
+
+(5, 8, 6, 4) =>  9
+(1, 2, 4, 6) => 11
+(1, 6)       =>  5
+( )          => -1
+(6)          => -1 (because only one kid)
+
+*/
+
+//***************Solution********************
+
+//Then simiplfied into one line by using an Lambda expression with Enumerable methods.
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⢀⣴⠟⠉⠀⠀⠀⠈⠻⣦⡀⠀⠀⠀⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣀⢀⣾⠿⠻⢶⣄⠀⠀⣠⣶⡿⠶⣄⣠⣾⣿⠗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⢻⣿⣿⡿⣿⠿⣿⡿⢼⣿⣿⡿⣿⣎⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⠉⠛⢛⣛⡉⠀⠀⠙⠛⠻⠛⠑⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣧⣤⣴⠿⠿⣷⣤⡤⠴⠖⠳⣄⣀⣹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣀⣟⠻⢦⣀⡀⠀⠀⠀⠀⣀⡈⠻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡿⠉⡇⠀⠀⠛⠛⠛⠋⠉⠉⠀⠀⠀⠹⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡟⠀⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠈⠑⠪⠷⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣦⣼⠛⢦⣤⣄⡀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠢⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠲⠖⠛⠻⣿⡿⠛⠉⠉⠻⠷⣦⣽⠿⠿⠒⠚⠋⠉⠁⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣾⠛⠁⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⠀⠀⠀
+⠀⠀⠀⠀⣰⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣑⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⠀⠀
+⠀⠀⠀⣰⣿⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣧⣄⠀⠀⠀⠀⠀⠀⢳⡀⠀
+⠀⠀⠀⣿⡾⢿⣀⢀⣀⣦⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⣫⣿⡿⠟⠻⠶⠀⠀⠀⠀⠀⢳⠀
+⠀⠀⢀⣿⣧⡾⣿⣿⣿⣿⣿⡷⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⢀⡴⢿⣿⣧⠀⡀⠀⢀⣀⣀⢒⣤⣶⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⠀⡾⠁⠙⣿⡈⠉⠙⣿⣿⣷⣬⡛⢿⣶⣶⣴⣶⣶⣶⣤⣤⠤⠾⣿⣿⣿⡿⠿⣿⠿⢿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⣸⠃⠀⠀⢸⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⠟⡉⠀⠀⠀⠈⠙⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⣿⠀⠀⢀⡏⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀⠀⠉⠠⠿⠟⠻⠟⠋⠉⢿⣿⣦⡀⢰⡀⠀⠀⠀⠀⠀⠀⠁
+⢀⣿⡆⢀⡾⠀⠀⠀⠀⣾⠏⢿⣿⣿⣿⣯⣙⢷⡄⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣻⢿⣷⣀⣷⣄⠀⠀⠀⠀⢸⠀
+⢸⠃⠠⣼⠃⠀⠀⣠⣾⡟⠀⠈⢿⣿⡿⠿⣿⣿⡿⠿⠿⠿⠷⣄⠈⠿⠛⠻⠶⢶⣄⣀⣀⡠⠈⢛⡿⠃⠈⢿⣿⣿⡿⠀⠀⠀⠀⠀⡀
+⠟⠀⠀⢻⣶⣶⣾⣿⡟⠁⠀⠀⢸⣿⢅⠀⠈⣿⡇⠀⠀⠀⠀⠀⣷⠂⠀⠀⠀⠀⠐⠋⠉⠉⠀⢸⠁⠀⠀⠀⢻⣿⠛⠀⠀⠀⠀⢀⠇
+⠀⠀⠀⠀⠹⣿⣿⠋⠀⠀⠀⠀⢸⣧⠀⠰⡀⢸⣷⣤⣤⡄⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡆⠀⠀⠀⠀⡾⠀⠀⠀⠀⠀⠀⢼⡇
+⠀⠀⠀⠀⠀⠙⢻⠄⠀⠀⠀⠀⣿⠉⠀⠀⠈⠓⢯⡉⠉⠉⢱⣶⠏⠙⠛⠚⠁⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⠀⠀⠀⠀⠀⠻⠄⠀⠀⠀⢀⣿⠀⢠⡄⠀⠀⠀⣁⠁⡀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⢀⣐⡟⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⠀⠀⠀
+*/
+//if candies length is 0 or 1, return -1
+//else get highest candies value and times the length of candies(max total), subtract by the current total
+using System.Linq;
+
+public class CandyProblem{
+	public static int GetMissingCandies(int[] candies) => 
+    candies.Length == 0 || candies.Length == 1 ? -1 : candies.Max() * candies.Length - candies.Sum();
+}
+
+//****************Sample Test*****************
+using NUnit.Framework;
+using System;
+
+[TestFixture]
+class CandyProblemTests
+{
+	[Test]
+	public void GetMissingCandiesTest_5_8_6_4()
+	{
+		int[] input = {5, 8, 6, 4};
+		int expected = 9;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void GetMissingCandiesTest_1_2_4_6()
+	{
+		int[] input = { 1, 2, 4, 6 };
+		int expected = 11;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void GetMissingCandiesTest_1_2()
+	{
+		int[] input = { 1, 2 };
+		int expected = 1;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void GetMissingCandiesTest_4_2()
+	{
+		int[] input = { 4, 2 };
+		int expected = 2;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void GetMissingCandiesTest_1_3_3_7()
+	{
+		int[] input = { 1, 3, 3, 7 };
+		int expected = 14;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void GetMissingCandiesTest_All7()
+	{
+		int[] input = { 7, 7, 7, 7 };
+		int expected = 0;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void GetMissingCandiesTest_17_20_50_61_42_44()
+	{
+		int[] input = { 17, 20, 50, 61, 42, 44 };
+		int expected = 132;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void GetMissingCandiesTest_0()
+	{
+		int[] input = { 0 };
+		int expected = -1;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void GetMissingCandiesTest_EmptyArray()
+	{
+		int[] input = { };
+		int expected = -1;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+
+	[Test]
+	public void GetMissingCandiesTest_4()
+	{
+		int[] input = { 4 };
+		int expected = -1;
+		int actual = CandyProblem.GetMissingCandies(input);
+		Assert.AreEqual(expected, actual);
+	}
+  
+	[Test]
+	public void GetMissingCandiesTest_Random()
+	{
+    Random rand = new Random();
+    for(int i=0; i<100; i++)
+    {
+      int length = rand.Next(42);
+      int[] input = new int[length];
+      int max = 0;
+      int sum = 0;
+      for(int j=0; j<length; j++)
+      {
+        int n = rand.Next(42);
+        input[j] = n;
+        if(n > max)
+        {
+          max = n;
+        }
+        sum += n;
+      }
+		  int expected = length > 1 ? max * length - sum : -1;
+    	int actual = CandyProblem.GetMissingCandies(input);
+      Assert.AreEqual(expected, actual);
+    }
+	}  
+}
