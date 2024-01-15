d9aa318be6937e018901c63f487f15557aff5e94
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-01-15 16:39:37 +0000

    Create 161_Sortable Shapes.cs

---
+++ 6 Kyu/161_Sortable Shapes.cs
@@ -0,0 +1,177 @@
+/*Challenge link:https://www.codewars.com/kata/586669a8442e3fc307000048/train/csharp
+Question:
+Although shapes can be very different by nature, they can be sorted by the size of their area.
+
+Task:
+
+Create different shapes that can be part of a sortable list. The sort order is based on the size of their respective areas:
+The area of a Square is the square of its side
+The area of a Rectangle is width multiplied by height
+The area of a Triangle is base multiplied by height divided by 2
+The area of a Circle is the square of its radius multiplied by π
+The area of a CustomShape is given
+
+The default sort order of a list of shapes is ascending on area size:
+var side = 1.1234D;
+var radius = 1.1234D;
+var base = 5D;
+var height = 2D;
+
+var shapes = new List<Shape>{ new Square(side),
+                            new Circle(radius),
+                            new Triangle(base, height) };
+shapes.Sort();
+Use the correct π constant for your circle area calculations:
+System.Math.PI
+
+*/
+
+//***************Solution********************
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⢀⣴⠟⠉⠀⠀⠀⠈⠻⣦⡀⠀⠀⠀⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣀⢀⣾⠿⠻⢶⣄⠀⠀⣠⣶⡿⠶⣄⣠⣾⣿⠗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⢻⣿⣿⡿⣿⠿⣿⡿⢼⣿⣿⡿⣿⣎⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⠉⠛⢛⣛⡉⠀⠀⠙⠛⠻⠛⠑⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣧⣤⣴⠿⠿⣷⣤⡤⠴⠖⠳⣄⣀⣹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣀⣟⠻⢦⣀⡀⠀⠀⠀⠀⣀⡈⠻⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡿⠉⡇⠀⠀⠛⠛⠛⠋⠉⠉⠀⠀⠀⠹⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡟⠀⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠈⠑⠪⠷⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣦⣼⠛⢦⣤⣄⡀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠢⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⠲⠖⠛⠻⣿⡿⠛⠉⠉⠻⠷⣦⣽⠿⠿⠒⠚⠋⠉⠁⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢦⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣾⠛⠁⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠤⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢣⠀⠀⠀
+⠀⠀⠀⠀⣰⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣑⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⠀⠀
+⠀⠀⠀⣰⣿⣁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣧⣄⠀⠀⠀⠀⠀⠀⢳⡀⠀
+⠀⠀⠀⣿⡾⢿⣀⢀⣀⣦⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⣫⣿⡿⠟⠻⠶⠀⠀⠀⠀⠀⢳⠀
+⠀⠀⢀⣿⣧⡾⣿⣿⣿⣿⣿⡷⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⢀⡴⢿⣿⣧⠀⡀⠀⢀⣀⣀⢒⣤⣶⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⠀⡾⠁⠙⣿⡈⠉⠙⣿⣿⣷⣬⡛⢿⣶⣶⣴⣶⣶⣶⣤⣤⠤⠾⣿⣿⣿⡿⠿⣿⠿⢿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⣸⠃⠀⠀⢸⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⠟⡉⠀⠀⠀⠈⠙⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⣿⠀⠀⢀⡏⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀⠀⠉⠠⠿⠟⠻⠟⠋⠉⢿⣿⣦⡀⢰⡀⠀⠀⠀⠀⠀⠀⠁
+⢀⣿⡆⢀⡾⠀⠀⠀⠀⣾⠏⢿⣿⣿⣿⣯⣙⢷⡄⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣻⢿⣷⣀⣷⣄⠀⠀⠀⠀⢸⠀
+⢸⠃⠠⣼⠃⠀⠀⣠⣾⡟⠀⠈⢿⣿⡿⠿⣿⣿⡿⠿⠿⠿⠷⣄⠈⠿⠛⠻⠶⢶⣄⣀⣀⡠⠈⢛⡿⠃⠈⢿⣿⣿⡿⠀⠀⠀⠀⠀⡀
+⠟⠀⠀⢻⣶⣶⣾⣿⡟⠁⠀⠀⢸⣿⢅⠀⠈⣿⡇⠀⠀⠀⠀⠀⣷⠂⠀⠀⠀⠀⠐⠋⠉⠉⠀⢸⠁⠀⠀⠀⢻⣿⠛⠀⠀⠀⠀⢀⠇
+⠀⠀⠀⠀⠹⣿⣿⠋⠀⠀⠀⠀⢸⣧⠀⠰⡀⢸⣷⣤⣤⡄⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡆⠀⠀⠀⠀⡾⠀⠀⠀⠀⠀⠀⢼⡇
+⠀⠀⠀⠀⠀⠙⢻⠄⠀⠀⠀⠀⣿⠉⠀⠀⠈⠓⢯⡉⠉⠉⢱⣶⠏⠙⠛⠚⠁⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⠀⠀⠀⠀⠀⠻⠄⠀⠀⠀⢀⣿⠀⢠⡄⠀⠀⠀⣁⠁⡀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⢀⣐⡟⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⢠⡇⠀⠀⠀⠀⠀
+*/
+//compare area of shapes, return 1, -1, 0 accordingly
+//https://learn.microsoft.com/en-us/dotnet/api/system.icomparable-1?view=net-8.0#remarks
+using System;
+public class Shape : IComparable<Shape>{
+    protected double Area = 0;
+    public int CompareTo(Shape other) => Area > other.Area ? 
+                                         1 : Area < other.Area ? 
+                                        -1 : 0;
+}
+
+//find area of shapes
+public class Square      : Shape{ public Square     (double s)                   {Area = s * s;}}
+public class Rectangle   : Shape{ public Rectangle  (double w, double h)         {Area = w * h;}}
+public class Circle      : Shape{ public Circle     (double r)                   {Area = r * r * Math.PI;}}
+public class Triangle    : Shape{ public Triangle   (double triBase, double h)   {Area = triBase * h * 0.5;}}
+public class CustomShape : Shape{ public CustomShape(double Area)                {this.Area = Area;}}
+
+
+
+//****************Sample Test*****************
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+[TestFixture]
+public class ShapesTests
+{
+    [Test]
+    public void ShapesAreSortableOnArea()
+    {
+        // Arrange
+        double width, height, triangleBase, side, radius, area;
+        Random random = new Random((int)DateTime.UtcNow.Ticks);
+
+        var expected = new List<Shape>();
+
+        area = 1.1234;
+        expected.Add(new CustomShape(area));
+
+        side = 1.1234;
+        expected.Add(new Square(side));
+
+        radius = 1.1234;
+        expected.Add(new Circle(radius));
+
+        triangleBase = 5;
+        height = 2;
+        expected.Add(new Triangle(triangleBase, height));
+
+        height = 3;
+        triangleBase = 4;
+        expected.Add(new Triangle(triangleBase, height));
+
+        width = 4;
+        expected.Add(new Rectangle(width, height));
+
+        area = 16.1;
+        expected.Add(new CustomShape(area));
+
+        var actual = expected.OrderBy(x => random.Next()).ToList(); 
+        
+        // Act
+        actual.Sort();
+
+        // Assert
+        for (var i = 0; i < 5; i++)
+            Assert.AreEqual(expected[i], actual[i]);
+    }
+
+    [Test]
+    public void RandomizedTest()
+    {
+        double side, width, height, triangleBase, area, radius;
+        var random = new Random((int)DateTime.UtcNow.Ticks);
+        Dictionary<double, Shape> shapes = new Dictionary<double, Shape>();
+        List<Shape> actual = new List<Shape>();
+        for (var i = 0; i < 25000; i++)
+        {
+            Shape shape;
+            switch(random.Next(0, 5))
+            {
+                case 0:
+                    side = random.NextDouble();
+                    shape = new Square(side);
+                    area = Math.Pow(side, 2);
+                    break;
+                case 1:
+                    width = random.NextDouble();
+                    height = random.NextDouble();
+                    shape = new Rectangle(width, height);
+                    area = width * height;
+                    break;
+                case 2:
+                    triangleBase = random.NextDouble();
+                    height = random.NextDouble();
+                    shape = new Triangle(triangleBase, height);
+                    area = (triangleBase * height) / 2;
+                    break;
+                case 3:
+                    radius = random.NextDouble();
+                    shape = new Circle(radius);
+                    area = (Math.Pow(radius, 2) * Math.PI);
+                    break;
+                default:
+                    area = random.NextDouble();
+                    shape = new CustomShape(area);
+                    break;
+            }
+            if (!shapes.ContainsKey(area))
+            {
+                shapes.Add(area, shape);
+                actual.Add(shape);
+            }
+        }
+        var expected = shapes.OrderBy(x => x.Key).Select(x => x.Value).ToList();
+        actual.Sort();
+        for (var j = 0; j < shapes.Count; j++)
+            Assert.AreEqual(expected[j], actual[j]);
+    }
+}
