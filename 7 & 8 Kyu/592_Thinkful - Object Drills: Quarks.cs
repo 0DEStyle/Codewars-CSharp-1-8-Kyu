1f8408e658d5f98a5932ee62bd6f12e7315b33f6
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-02-18 21:25:41 +0000

    Create 592_Thinkful - Object Drills: Quarks.cs

---
+++ 7 & 8 Kyu/592_Thinkful - Object Drills: Quarks.cs
@@ -0,0 +1,115 @@
+/*Challenge link:https://www.codewars.com/kata/5882b052bdeafec15e0000e6/train/csharp
+Question:
+Background
+You're modelling the interaction between a large number of quarks and have decided to create a Quark class so you can generate your own quark objects.
+
+Quarks are fundamental particles and the only fundamental particle to experience all four fundamental forces.
+
+Your task
+Your Quark class should allow you to create quarks of any valid color ("red", "blue", and "green") and any valid flavor ('up', 'down', 'strange', 'charm', 'top', and 'bottom').
+
+Every quark has the same baryon_number (BaryonNumber in C#): 1/3.
+
+Every quark should have an .interact() (.Interact() in C#) method that allows any quark to interact with another quark via the strong force. When two quarks interact they exchange colors.
+
+Example
+>>> Quark q1 = new Quark("red", "up");
+>>> q1.Color;
+"red"
+>>> Quark q2 = new Quark("blue", "strange");
+>>> q2.Color;
+"blue"
+>>> q2.BaryonNumber;
+0.3333333333333333
+>>> q1.Interact(q2);
+>>> q1.Color;
+"blue"
+>>> q2.Color;
+"red"
+*/
+
+//***************Solution********************
+/*
+⬜⬜⬜⬜⬜⬜⬜⬛⬛⬛⬛⬛⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜
+⬜⬜⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜
+⬜⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜
+⬜⬜⬜⬜⬛🟨🟨🟨🟨⬛⬛⬜🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜
+⬜⬜⬜⬜⬛🟨🟨🟨🟨⬛⬛⬛🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜
+⬜⬜⬜⬜⬛⬛🟨🟨🟨⬛⬛⬛🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜
+⬜⬜⬜⬛🟨🟨⬛🟨🟨🟨🟨🟨🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜
+⬜⬛⬛🟨🟨🟨🟨⬛🟨🟨🟨🟨🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜
+⬛🟨🟨🟨🟨🟧🟧⬛🟨🟨🟨🟨🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜
+⬜⬛🟧🟧🟧🟧🟧⬛⬛🟨🟨🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬛
+⬜⬜⬛⬛⬛⬛⬛🟨🟨🟨🟨🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬛⬛
+⬜⬜⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨⬛🟨⬛⬛⬛⬛⬛⬜⬜⬜⬜⬜⬛🟨⬛
+⬜⬜⬜⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛⬛⬛⬛⬛🟨🟨⬛
+⬜⬜⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛
+⬜⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛
+⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛⬛⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛
+⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛🟨🟨🟨🟨🟨🟨🟨🟨⬛⬜
+⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛⬛🟨🟨🟨🟨🟨🟨⬛⬜
+⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛🟨🟨🟨🟨🟨🟨⬛⬜
+⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛🟨🟨🟨🟨🟨🟨🟨⬛⬜
+⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛🟨🟨🟨🟨🟨🟨🟨🟨⬛⬜
+⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛⬛⬛🟨🟨🟨🟨🟨🟨🟨🟨⬛⬜⬜
+⬜⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛⬛⬜⬜⬜
+⬜⬜⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛⬜⬜⬜⬜⬜
+⬜⬜⬜⬜⬜⬜⬜⬛🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨🟨⬛⬜⬜⬜⬜⬜⬜
+⬜⬜⬜⬜⬜⬜⬜⬜⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬜⬜⬜⬜⬜⬜⬜
+*/
+public class Quark {
+  public string Color; //"red", "blue", and "green"
+  public string Flavor; //'up', 'down', 'strange', 'charm', 'top', and 'bottom'
+  public Quark(string color, string flavor){ Color = color; Flavor = flavor; }
+  
+  public double BaryonNumber => 1 / 3.0;  // 1/3 fraction
+  public void Interact(Quark quark1) => (Color, quark1.Color) =  (quark1.Color, Color); //When two quarks interact they exchange colors.
+}
+
+//****************Sample Test*****************
+namespace Solution {
+  using NUnit.Framework;
+  using System;
+
+  [TestFixture]
+  public class SolutionTest
+  {
+    [Test]
+    public void SampleTest()
+    {
+      Quark q1 = new Quark("red", "up");
+      Quark q2 = new Quark("blue", "strange");
+      Assert.AreEqual("red", q1.Color);
+      Assert.AreEqual("strange", q2.Flavor);
+      Assert.AreEqual(1 / 3d, q2.BaryonNumber);
+      
+      q1.Interact(q2);
+      Assert.AreEqual("blue", q1.Color);
+    }
+    
+    private static Random rnd = new Random();
+    
+    [Test]
+    public void RandomTest()
+    {
+      string[] colors = new string[] {"red", "blue", "green"};
+      string[] flavors = new string[] {"up", "down", "top", "bottom", "strange", "charm"}; 
+      
+      for (int i = 0; i < 20; ++i)
+      {
+        string c1 = colors[rnd.Next(0, colors.Length)];
+        string c2 = colors[rnd.Next(0, colors.Length)];
+        string f1 = flavors[rnd.Next(0, flavors.Length)];
+        string f2 = flavors[rnd.Next(0, flavors.Length)];
+        Quark q1 = new Quark(c1, f1);
+        Quark q2 = new Quark(c2, f2);
+        Assert.AreEqual(c1, q1.Color);
+        Assert.AreEqual(f1, q1.Flavor);
+        Assert.AreEqual(1 / 3d, q1.BaryonNumber);
+        q1.Interact(q2);
+        Assert.AreEqual(c2, q1.Color);
+        Assert.AreEqual(c1, q2.Color);
+      }
+    }
+  }
+}
